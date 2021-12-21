e56728f3c28f8be870e7a8388b7404592d45195b
Georg Streich <streichg@ethz.ch>
2021-12-21 10:50:21 -0500

    Update params

--- include/params.h
+++ include/params.h
@@ -1,5 +1,7 @@
-static double alpha = 1e-4;
-static double beta = 0.0;
-static double dt = 1e-4;
-static double k_selected = 5e5;
+static double alpha = 1e-1;
+static double beta = 0.8;
+static double dt = 1e-2;
+static double k_selected = 1e3;
+static double s_friction = 1e1;
 
+static bool use_quadratic = true;
